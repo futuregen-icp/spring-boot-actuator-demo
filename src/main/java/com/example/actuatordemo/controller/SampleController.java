6d0229a50de9085e0b32a82279599d760ac4a848
dslee1371 <37610050+dslee1371@users.noreply.github.com>
2022-01-10 15:45:26 +0900

    Update SampleController.java

--- src/main/java/com/example/actuatordemo/controller/SampleController.java
+++ src/main/java/com/example/actuatordemo/controller/SampleController.java
@@ -16,7 +16,7 @@ public class SampleController {
     }
 
     @GetMapping("/slowApi")
-    public String timeConsumingAPI(@RequestParam(value = "delay", defaultValue = "5000") Integer delay) throws InterruptedException {
+    public String timeConsumingAPI(@RequestParam(value = "delay", defaultValue = "0") Integer delay) throws InterruptedException {
         if(delay == 0) {
             Random random = new Random();
             delay = random.nextInt(10);
